f9e78f484d9a1315e3aac06ad49e86354620e4d0
Rylan Merritt <rmerritt@sageisg.com>
2025-07-22 23:14:53 -0600

    Added comments

--- ingest_input_plugin/interface.go
+++ ingest_input_plugin/interface.go
@@ -7,6 +7,6 @@ import (
 // InputPlugin defines the common interface that all input plugins must implement.
 type InputPlugin interface {
 	Start(config map[string]interface{}, queues []logwarp_common.IngestQueueConfig) error // Start listening on configured ports or connections
-	Stop()                                                                                // Stop listening and clean up resources
+	Stop() error                                                                          // Stop listening and clean up resources
 	Flush() error                                                                         // Flush the current buffer in the input plugin
 }
